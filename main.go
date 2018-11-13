5bb20a3e7a4c4b28d32c7109f1148240679d7d55
Chady Kamar <chady.kamar@gmail.com>
2018-11-13 16:51:28 -0500

    87ms without concurrency

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 	"time"
 )
 
@@ -32,6 +37,67 @@ func main() {
 //	number of line deleted
 //	list of function calls seen in the diffs and their number of calls
 func compute() *result {
+	root := "./diffs"
 
-	return nil
+	re, err := regexp.Compile("[A-Za-z_][A-Za-z0-9_]+\\(")
+	if err != nil {
+		fmt.Println(err)
+	}
+	var regions int
+	var linesAdded int
+	var linesDeleted int
+	var files []string
+	functionCalls := make(map[string]int)
+
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if strings.HasPrefix(line, "@@") {
+				regions++
+			} else if strings.HasPrefix(line, "+++") {
+				// If the file has been renamed or copied we keep the newer name and get rid
+				// of the prefix "+++ b/"
+				files = append(files, line[6:])
+			} else if strings.HasPrefix(line, "+") {
+				linesAdded++
+			} else if strings.HasPrefix(line, "-") && !strings.HasPrefix(line, "---") {
+				linesDeleted++
+			} else {
+				matches := re.FindAllString(line, -1)
+				if matches == nil {
+					continue
+				}
+				for _, match := range matches {
+					// We'll keep only the function name i.e. remove the parentheses and params
+					functionCall := match[:len(match)-1]
+
+					if _, ok := functionCalls[functionCall]; ok {
+						functionCalls[functionCall]++
+					} else {
+						functionCalls[functionCall] = 1
+					}
+				}
+			}
+		}
+		return nil
+	})
+
+	return &result{files, regions, linesAdded, linesDeleted, functionCalls}
+
 }
