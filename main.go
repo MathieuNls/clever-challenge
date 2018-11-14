d820bfa15411f278b1aa734d154d884ac32615ef
Chady Kamar <chady.kamar@gmail.com>
2018-11-14 12:45:29 -0500

    Parallelized with goroutines 62ms

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -43,12 +44,20 @@ func compute() *result {
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	var regions int
 	var linesAdded int
 	var linesDeleted int
 	var files []string
 	functionCalls := make(map[string]int)
 
+	// Reader wg
+	var rwg sync.WaitGroup
+
+	// lines receives the lines of the diff files from their respective goroutines
+	lines := make(chan string, 50)
+
+	// Line reader, one goroutine spawned per file
 	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -57,46 +66,126 @@ func compute() *result {
 		if info.IsDir() {
 			return nil
 		}
-
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			line := scanner.Text()
-
-			if strings.HasPrefix(line, "@@") {
-				regions++
-			} else if strings.HasPrefix(line, "+++") {
-				// If the file has been renamed or copied we keep the newer name and get rid
-				// of the prefix "+++ b/"
-				files = append(files, line[6:])
-			} else if strings.HasPrefix(line, "+") {
-				linesAdded++
-			} else if strings.HasPrefix(line, "-") && !strings.HasPrefix(line, "---") {
-				linesDeleted++
-			} else {
-				matches := re.FindAllString(line, -1)
-				if matches == nil {
-					continue
-				}
-				for _, match := range matches {
-					// We'll keep only the function name i.e. remove the parentheses and params
-					functionCall := match[:len(match)-1]
-
-					if _, ok := functionCalls[functionCall]; ok {
-						functionCalls[functionCall]++
-					} else {
-						functionCalls[functionCall] = 1
-					}
-				}
+		rwg.Add(1)
+		go func() {
+			defer rwg.Done()
+			file, err := os.Open(path)
+			if err != nil {
+				return
 			}
-		}
+			defer file.Close()
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				lines <- scanner.Text()
+			}
+		}()
+
 		return nil
 	})
+
+	// Clean up readers
+	go func() {
+		rwg.Wait()
+		close(lines)
+	}()
+
+	// "Processor" wg
+	var pwg sync.WaitGroup
+
+	regionsChan := make(chan int)
+	linesAddedChan := make(chan int)
+	linesDeletedChan := make(chan int)
+	filesChan := make(chan string)
+	functionCallsChan := make(chan string)
+
+	// Receive lines and process them, then send the result to the appropriate channel defined above.
+	pwg.Add(1)
+	go func() {
+		defer pwg.Done()
+		for line := range lines {
+
+			pwg.Add(1)
+			go func(line string) {
+				defer pwg.Done()
+				if strings.HasPrefix(line, "@@") {
+					regionsChan <- 1
+				} else if strings.HasPrefix(line, "+++") {
+					// If the file has been renamed or copied we keep the newer name and get rid
+					// of the prefix "+++ b/"
+					filesChan <- line[6:]
+				} else if strings.HasPrefix(line, "+") {
+					linesAddedChan <- 1
+				} else if strings.HasPrefix(line, "-") && !strings.HasPrefix(line, "---") {
+					linesDeletedChan <- 1
+				} else if !strings.HasPrefix(line, "-") && !strings.HasPrefix(line[1:], "#") &&
+					!strings.HasPrefix(line[1:], "//") && !strings.HasPrefix(line[1:], "/*") {
+
+					matches := re.FindAllString(line, -1)
+					for _, match := range matches {
+						// We'll keep only the function name i.e. remove the parentheses and params
+						functionCall := match[:len(match)-1]
+						functionCallsChan <- functionCall
+					}
+				}
+			}(line)
+		}
+	}()
+
+	// CLose the processing channels
+	go func() {
+		pwg.Wait()
+		close(regionsChan)
+		close(linesAddedChan)
+		close(linesDeletedChan)
+		close(filesChan)
+		close(functionCallsChan)
+	}()
+
+	// Workers for each type
+	var wwg sync.WaitGroup
+
+	wwg.Add(5)
+
+	go func() {
+		defer wwg.Done()
+		for range regionsChan {
+			regions++
+		}
+	}()
+
+	go func() {
+		defer wwg.Done()
+		for range linesAddedChan {
+			linesAdded++
+		}
+	}()
+
+	go func() {
+		defer wwg.Done()
+		for range linesDeletedChan {
+			linesDeleted++
+		}
+	}()
+
+	go func() {
+		defer wwg.Done()
+		for file := range filesChan {
+			files = append(files, file)
+		}
+	}()
+
+	go func() {
+		defer wwg.Done()
+		for functionCall := range functionCallsChan {
+			if _, ok := functionCalls[functionCall]; ok {
+				functionCalls[functionCall]++
+			} else {
+				functionCalls[functionCall] = 1
+			}
+		}
+	}()
+
+	wwg.Wait()
 
 	return &result{files, regions, linesAdded, linesDeleted, functionCalls}
 
