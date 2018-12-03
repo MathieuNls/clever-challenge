4b75defc8d9db21f08695f145de06baf035a49c2
Tormyst <tormyst@github.com>
2018-12-03 18:07:34 -0400

    Replacing the boilerplate go code with a Rust Cargo project.
    Also implemented += on result as part of this change.

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+#![allow(non_snake_case)]
+use std::time::Instant;
+
+mod result;
+use result::Result;
+
+// timeTrack has been modified in the conversion from go to Rust.
+// Unfortunetly, the time library has many direct time functions
+// still marked as nightly.
+fn timeTrack(start: Instant, string: &'static str) {
+    let elapsed = start.elapsed();
+    print!("{} took {} seconds and {} nanoseconds\n",
+           string,
+           elapsed.as_secs(),
+           elapsed.subsec_nanos());
+}
+
+// main is the entry point of our Rust program.
+//
+// The go line: defer timeTrack(time.Now(), "compute diff")
+// translates as our first and last lines.  Rust has no defer
+// as that is memory that is held for the duration of the function
+// secretly and against the visibility that rust strives for.
+//
+// It also calls the compute and Display method of the returned struct
+// to stdout.
+fn main() {
+    let now = Instant::now();
+
+    println!("{}", compute());
+
+    timeTrack(now, "compute diff");
+}
+
+// compute parses the git diffs in ./diffs and returns
+// a result struct that contains all the relevant informations
+// about these diffs
+//  list of files in the diffs
+//  number of regions
+//  number of line added
+//  number of line deleted
+//  list of function calls seen in the diffs and their number of calls
+fn compute() -> Result {
+    Result::empty()
+}
---
+++ src/result.rs
@@ -0,0 +1,75 @@
+use std::fmt;
+use std::collections::{HashSet, HashMap};
+use std::ops::AddAssign;
+
+// result contains an analysis of a set of commits
+pub struct Result {
+    // The name of the files seen
+    files: HashSet<String>,
+    // How many regions we have (i.e. seperated by @@)
+    regions: usize,
+    // How many lines were added total
+    lineAdded: usize,
+    // How many lines were deleted total
+    lineDeleted: usize,
+    // How many times the function seen in the code are called
+    functionCalls: HashMap<String, usize>,
+}
+
+impl Result {
+    // Creates an empty Result struct.
+    pub fn empty() -> Self {
+        Self::new(HashSet::new(), 0, 0, 0, HashMap::new())
+    }
+
+    // Constructor for a Result struct that expects everything to be handed to it.  
+    pub fn new(files: HashSet<String>,
+               regions: usize,
+               lineAdded: usize,
+               lineDeleted: usize,
+               functionCalls: HashMap<String, usize>)
+               -> Self {
+        Result {
+            files,
+            regions,
+            lineAdded,
+            lineDeleted,
+            functionCalls,
+        }
+    }
+}
+
+// Implementation of the formating system in rust ensuring that the structure may be 
+// printed in a way close to that of the print statment provided in the go structure.
+impl fmt::Display for Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Files: \n")?;
+        for file in &self.files {
+            write!(f, "    -{}\n", file)?;
+        }
+        write!(f, "Regions: {}\n", self.regions)?;
+        write!(f, "LA: {}\n", self.lineAdded)?;
+        write!(f, "LD: {}\n", self.lineDeleted)?;
+
+        write!(f, "Functions calls: \n")?;
+        for (key, value) in &self.functionCalls {
+            write!(f, "   {}: {}\n", key, value)?;
+        }
+        Ok(())
+    }
+}
+
+// Iplementation of the += opperator which is useful syntacticaly to clearly demonstrait
+// how results are combined.
+impl AddAssign for Result {
+    fn add_assign(&mut self, mut other: Result) {
+        other.files.drain().for_each(|file| {self.files.insert(file);});
+        self.regions += other.regions;
+        self.lineAdded += other.lineAdded;
+        self.lineDeleted += other.lineDeleted;
+        other.functionCalls.drain().for_each(|(key, value)| {
+            let to_add = *self.functionCalls.get(&key).unwrap_or(&0);
+            self.functionCalls.insert(key, value + to_add);
+        });
+    }
+}
