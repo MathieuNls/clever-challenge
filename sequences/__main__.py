2618ddb666e94b56b7a4ae5fc5ae65dbbab6e36b
Paul-Antoine Dumergue <paul-antoine.dumergue@outlook.com>
2019-08-03 14:01:05 -0400

    Iterate on model

--- sequences/__main__.py
+++ sequences/__main__.py
@@ -1,98 +1,132 @@
-import numpy
 import numpy as np
 import pandas as pd
-from keras.layers import LSTM, Dense
-from keras.models import Sequential
-from sklearn.metrics import mean_squared_error
+from keras import Sequential
+from keras.layers import Dropout, LSTM, Dense
 from matplotlib import pyplot
+from sklearn.decomposition import PCA
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
 
 def predict_sequence():
     sample = pd.read_csv("../seq/sample.csv")
     # ressources = pd.read_csv("../seq/res.csv")
     sample = prepare_data(sample)
 
-    # split source data in training and validation sets
-    x_train, y_train, x_validate, y_validate = split_set(sample, training_percentage=0.90)
+    # create series and split in training and validation sets
+    x_series, y_series = create_series(sample, 30, 2)
+    x_train, x_validate, y_train, y_validate = train_test_split(x_series, y_series, test_size=0.2)
 
     print("x_train", x_train.shape)
     print("y_train", y_train.shape)
     print("x_validate", x_validate.shape)
     print("y_validate", y_validate.shape)
 
+    # Now we can train and check our model
     model = train_model(x_train, y_train, x_validate, y_validate)
+
+    check_model(model, x_train, y_train)
     check_model(model, x_validate, y_validate)
 
 
 def prepare_data(data):
+    """
+    Prepare the data before training
+    :param data: raw data
+    :return: prepared data
+    """
     data = data.drop("event_id", axis=1)
-    return data
+    data = data.drop("timestamp", axis=1)
+
+    prepared_data = data.values
+
+    attr = data.values[:, 1:]
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    attr = scaler.fit_transform(attr)
+
+    # pca = PCA(n_components=0.90, svd_solver='full')
+    # attr = pca.fit_transform(attr)
+
+    prepared_data = np.insert(attr, 0, values=prepared_data[:, 0], axis=1)
+    return prepared_data
 
 
-def extract_used_ressources(data):
-    event = data.values[:, 0]
-    resources = data.values[:, 1]
+def split_set(x, y, training_percentage=0.75):
+    series_count = x.shape[0]
+    split_index = int(series_count * training_percentage)
 
-    print(len(numpy.unique(event)))
-    print(len(numpy.unique(resources)))
+    x_train = x[:split_index]
+    y_train = y[:split_index]
 
-
-def split_set(data_set, training_percentage=0.75):
-    event_count = data_set.shape[0]
-    train_size = int(event_count * training_percentage)
-
-    classes = data_set.pop("class")
-
-    x_train = data_set[:train_size].values
-    y_train = classes[:train_size].values
-
-    x_validate = data_set[train_size:].values
-    y_validate = classes[train_size:].values
-
-    # We need to project our data in 3D for LSTM
-    x_train = np.reshape(x_train, (x_train.shape[0], 1, x_train.shape[1]))
-    x_validate = np.reshape(x_validate, (x_validate.shape[0], 1, x_validate.shape[1]))
+    x_validate = x[split_index:]
+    y_validate = y[split_index:]
 
     return x_train, y_train, x_validate, y_validate
 
 
+def create_series(data, used=1, predicted=1):
+    """
+    Splits the data in prediction sets as follows
+    x is the vector of data used for the prediction
+    y is the vector of class that happen after x
+
+    :param data : pandas DataFrame, raw data
+    :param used : number of event to use to predict
+    :param predicted : number of event we want to predict
+    """
+    data_count, attribute_count = data.shape
+    series_count = data_count - used - predicted
+
+    x = np.zeros(shape=(series_count, used, attribute_count))
+    y = np.zeros(shape=(series_count, predicted))
+
+    for i in range(0, series_count):
+        range_start = i
+        used_range_end = range_start + used
+        predicted_range_end = used_range_end + predicted
+
+        x[i, :, :] = data[range_start:used_range_end]
+        y[i, ] = data[used_range_end:predicted_range_end, 0]
+
+    return x, y
+
+
 def train_model(x_train, y_train, x_validate, y_validate):
+    """
+    Train the tf model with given sets
+    :param x_train: training set attributes values
+    :param y_train: training set class labels
+    :param x_validate: training set attributes values
+    :param y_validate: training set class labels
+    :returns the trained model
+    """
     model = Sequential()
-    model.add(LSTM(units=50, input_shape=(x_train.shape[1], x_train.shape[2])))
-    # model.add(Dropout(0.2))
-    # model.add(LSTM(units=50, return_sequences=True))
+    model.add(LSTM(units=64, input_shape=(x_train.shape[1], x_train.shape[2]), kernel_initializer='normal'))
+    model.add(Dense(y_train.shape[1], activation='sigmoid', kernel_initializer='normal'))
 
-    model.add(Dense(units=1))
-    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print(model.summary())
 
-    model.fit(x_train, y_train, epochs=1, batch_size=72, validation_data=(x_validate, y_validate))
-    history = model.fit(x_train, y_train, epochs=50, batch_size=72, validation_data=(x_validate, y_validate), verbose=2,
-                        shuffle=False)
+    history = model.fit(x_train, y_train, epochs=1, batch_size=64, validation_data=(x_validate, y_validate), verbose=2)
+
     # plot history
     pyplot.plot(history.history['loss'], label='train')
     pyplot.plot(history.history['val_loss'], label='test')
     pyplot.legend()
     pyplot.show()
+
     return model
 
 
 def check_model(model, x_eval, y_eval):
-    y_predicted = model.predict(x_eval)
-    print(y_predicted.shape, y_eval.shape)
-    # calculate RMSE
-    diff = 0
-    rmse = np.sqrt(mean_squared_error(y_predicted, y_eval))
-    for i in range(len(y_predicted)):
-        if y_predicted[i] != y_eval[i]:
-            diff += 1
+    for x in x_eval:
+        print(model.predict(x.reshape(1, x.shape[0], x.shape[1])))
+    # y_predicted = model.predict(x_eval)
 
-    print(diff, y_predicted.shape)
-    print('Test RMSE: %.3f' % rmse)
-
-
-def predict_next(model, x):
-    pass
+    # print(accuracy_score(y_eval, y_predicted))
+    # print(confusion_matrix(y_eval, y_predicted))
 
 
 if __name__ == "__main__":
-    t = 1
     predict_sequence()
