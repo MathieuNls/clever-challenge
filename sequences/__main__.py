107739e76a9d74c07f55beeb371ba64e287e71af
Paul-Antoine Dumergue <paul-antoine.dumergue@outlook.com>
2019-08-04 13:34:30 -0400

    Iterate on model

--- sequences/__main__.py
+++ sequences/__main__.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 from keras import Sequential
-from keras.layers import Dropout, LSTM, Dense
+from keras.layers import LSTM, Dense
 from matplotlib import pyplot
-from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
@@ -15,7 +16,7 @@ def predict_sequence():
     sample = prepare_data(sample)
 
     # create series and split in training and validation sets
-    x_series, y_series = create_series(sample, 30, 2)
+    x_series, y_series = create_series(sample, 5, 1)
     x_train, x_validate, y_train, y_validate = train_test_split(x_series, y_series, test_size=0.2)
 
     print("x_train", x_train.shape)
@@ -26,8 +27,8 @@ def predict_sequence():
     # Now we can train and check our model
     model = train_model(x_train, y_train, x_validate, y_validate)
 
-    check_model(model, x_train, y_train)
-    check_model(model, x_validate, y_validate)
+    check_model(model, x_train, y_train, name="Training set")
+    check_model(model, x_validate, y_validate, name="Validation set")
 
 
 def prepare_data(data):
@@ -40,29 +41,18 @@ def prepare_data(data):
     data = data.drop("timestamp", axis=1)
 
     prepared_data = data.values
+    classes = prepared_data[:, 0]
+    features = data.values[:, 1:]
 
-    attr = data.values[:, 1:]
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    attr = scaler.fit_transform(attr)
+    min_max_scaler = MinMaxScaler(feature_range=(0, 1))
+    features = min_max_scaler.fit_transform(features)
 
-    # pca = PCA(n_components=0.90, svd_solver='full')
-    # attr = pca.fit_transform(attr)
+    # sel = SelectFromModel(RandomForestClassifier(n_estimators=100))
+    # sel.fit(features, classes)
+    # features = sel.transform(features)
 
-    prepared_data = np.insert(attr, 0, values=prepared_data[:, 0], axis=1)
+    prepared_data = np.insert(features, 0, values=classes, axis=1)
     return prepared_data
-
-
-def split_set(x, y, training_percentage=0.75):
-    series_count = x.shape[0]
-    split_index = int(series_count * training_percentage)
-
-    x_train = x[:split_index]
-    y_train = y[:split_index]
-
-    x_validate = x[split_index:]
-    y_validate = y[split_index:]
-
-    return x_train, y_train, x_validate, y_validate
 
 
 def create_series(data, used=1, predicted=1):
@@ -102,13 +92,13 @@ def train_model(x_train, y_train, x_validate, y_validate):
     :returns the trained model
     """
     model = Sequential()
-    model.add(LSTM(units=64, input_shape=(x_train.shape[1], x_train.shape[2]), kernel_initializer='normal'))
+    model.add(LSTM(units=64, input_shape=(x_train.shape[1], x_train.shape[2]), kernel_initializer='normal', activation="relu"))
     model.add(Dense(y_train.shape[1], activation='sigmoid', kernel_initializer='normal'))
 
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     print(model.summary())
 
-    history = model.fit(x_train, y_train, epochs=1, batch_size=64, validation_data=(x_validate, y_validate), verbose=2)
+    history = model.fit(x_train, y_train, epochs=100, batch_size=64, validation_data=(x_validate, y_validate), verbose=2)
 
     # plot history
     pyplot.plot(history.history['loss'], label='train')
@@ -119,13 +109,13 @@ def train_model(x_train, y_train, x_validate, y_validate):
     return model
 
 
-def check_model(model, x_eval, y_eval):
-    for x in x_eval:
-        print(model.predict(x.reshape(1, x.shape[0], x.shape[1])))
-    # y_predicted = model.predict(x_eval)
+def check_model(model, x_eval, y_eval, name):
+    y_predicted = model.predict_classes(x_eval)
 
-    # print(accuracy_score(y_eval, y_predicted))
-    # print(confusion_matrix(y_eval, y_predicted))
+    acc = accuracy_score(y_eval, y_predicted)
+    print("Accuracy on {0} : {1}%".format(name, int(acc*100)))
+    print("Confusion matrix for", name)
+    print(confusion_matrix(y_eval, y_predicted))
 
 
 if __name__ == "__main__":
